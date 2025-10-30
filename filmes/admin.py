e90e031238f1246bebbeebe346f14c0d3ed9a245
josepatobr <169946707+josepatobr@users.noreply.github.com>
2025-10-29 21:55:07 -0300

    adicionando o home

--- filmes/admin.py
+++ filmes/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Movies
+from .models import Movies, Gender
 
 admin.site.register(Movies)
+admin.site.register(Gender)
+
--- filmes/models.py
+++ filmes/models.py
@@ -6,19 +6,22 @@ class Gender(models.Model):
     genders = models.CharField(max_length=50, null=False, blank=False)
     slug = models.CharField(max_length=50, unique=True, null=False, blank=False)
 
-    def _str_(self):
+    def __str__(self):
         return self.genders
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.genders)
         super().save(*args, **kwargs)
 
+    class Meta:
+        verbose_name = "Gender"
+        verbose_name_plural = "Genders"
 
 class Movies(models.Model):
     movie = models.FileField(upload_to="movies", null=True, blank=True)
     name_movie = models.CharField(max_length=100, blank=False, null=False)
     description = models.CharField(max_length=1000, blank=False, null=False)
-    gender = models.ManyToManyField(Gender)
+    gender = models.ManyToManyField(Gender, help_text="para adicionar mais de 1 genero, voce precisa segurar o ctrl")
     poster = models.ImageField(upload_to="posters", blank=False, null=False)
     release_date = models.DateField(blank=True, null=True)
     watch_count = models.PositiveIntegerField(default=0)
@@ -29,3 +32,7 @@ class Movies(models.Model):
     def view_counter(self):
         self.watch_count += 1
         self.save()
+
+    class Meta:
+        verbose_name = "Movie"
+        verbose_name_plural = "Movies"
