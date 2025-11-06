a1f050e90d5a7b447805db0d59450996fc45278b
josepatobr <169946707+josepatobr@users.noreply.github.com>
2025-11-06 11:08:29 -0300

    arrumando o html

--- static/js/movie.js
+++ static/js/movie.js
@@ -1,22 +1,39 @@
-const movieId = { movie_id };
+const movieId = video.dataset.movieId;
 const video = document.getElementById('Video');
 
-video.addEventListener('play', () => {
-    const hasWatched = localStorage.getItem(`watched_${movieId}`);
+let tempoAssistido = 0;
+let intervalo;
 
+
+video.addEventListener('play', () => {
+    intervalo = setInterval(() => {
+        tempoAssistido += 1;
+    }, 1000);
+
+    const hasWatched = localStorage.getItem(`watched_${movieId}`);
     if (!hasWatched) {
         fetch("{% url 'CounterView' movie.id %}", {
-        method: 'POST',
-        headers: {
-            'X-CSRFToken': '{{ csrf_token }}',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({})
+            method: 'POST',
+            headers: {
+                'X-CSRFToken': '{{ csrf_token }}',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({})
         })
         .then(res => res.json())
         .then(data => {
-        localStorage.setItem(`watched_${movieId}`, true);
-        console.log('Contador atualizado:', data.watch_count);
+            localStorage.setItem(`watched_${movieId}`, true);
+            console.log('Contador atualizado:', data.watch_count);
         });
     }
+});
+
+video.addEventListener('pause', () => {
+    clearInterval(intervalo);
+    console.log(`Assistido até agora: ${tempoAssistido} segundos`);
+});
+
+video.addEventListener('ended', () => {
+    clearInterval(intervalo);
+    console.log(`Vídeo completo assistido: ${tempoAssistido} segundos`);
 });
