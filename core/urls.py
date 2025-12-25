1dd5bb01dfd4b0d5174da30b96770f01dea4a93c
josepatobr <josecarlosrodriguesrod.pinto@gmail.com>
2025-12-25 15:20:22 -0300

    mudanças nos filmes

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .api import api
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("cadastro.urls")),
+    path("api", api.urls),
     path("", include("home.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
