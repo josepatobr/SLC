37495d47f7bae052d48221c5391f9a63ff57271e
josepatobr <josecarlosrodriguesrod.pinto@gmail.com>
2026-01-01 16:42:24 -0300

    mudanças no login

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,12 @@
-from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from .api import api
+from django.contrib import admin
+from allauth.account.decorators import secure_admin_login
+
+admin.autodiscover(),
+admin.site.login = secure_admin_login(admin.site.login)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
