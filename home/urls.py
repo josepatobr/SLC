7e121d51c1622d032b653f9659800b2789c7e828
josepatobr <josecarlosrodriguesrod.pinto@gmail.com>
2026-01-03 20:25:09 -0300

    mudanças no login

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("home/", views.home, name="home"),
+    path("", views.home, name="home"),
     path("movie/<int:id>/", views.movie, name="movie"),
     path("serie/<int:id>/", views.serie, name="serie"),
     path("profile/<int:id>", views.profile, name="profile"),
