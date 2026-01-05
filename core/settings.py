a11c4d69e3db0342a1ce4bd002967ea2f7945872
josepatobr <josecarlosrodriguesrod.pinto@gmail.com>
2026-01-05 19:10:13 -0300

    mudanças no login

--- core/settings.py
+++ core/settings.py
@@ -135,7 +135,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # email
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
