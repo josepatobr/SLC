7cfb05c3211406e92bbd915dbee36caf53f5a1ab
josepatobr <josecarlosrodriguesrod.pinto@gmail.com>
2026-01-05 18:42:27 -0300

    mudanças no login

--- users/models.py
+++ users/models.py
@@ -4,12 +4,13 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    username = models.CharField(max_length=200, unique=False, null=True, blank=True)
+    username = None
     full_name = models.CharField(max_length=200, unique=False, null=True, blank=True)
     profile_picture = models.ImageField(
         upload_to="profile_picture", blank=True, null=True
     )
-
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return str(self.username) if self.username else str(self.email)
