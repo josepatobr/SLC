f0fe3df2702fe402e6156834419b817448f85ba2
josepatobr <josecarlosrodriguesrod.pinto@gmail.com>
2026-01-06 12:38:13 -0300

    mudanças no login

---
+++ users/managers.py
@@ -0,0 +1,43 @@
+from typing import TYPE_CHECKING
+
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import UserManager as DjangoUserManager
+from django.utils.translation import gettext_lazy as _
+
+if TYPE_CHECKING:
+    from .models import CustomUser
+
+
+class UserManager(DjangoUserManager["CustomUser"]):
+    """Custom manager for the User model."""
+
+    def _create_user(self, email: str, password: str | None, **extra_fields):
+        """
+        Create and save a user with the given email and password.
+        """
+        if not email:
+            msg = _("The Email must be set")
+            raise ValueError(msg)
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email: str, password: str | None = None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email: str, password: str | None = None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            msg = _("Superuser must have is_staff=True.")
+            raise ValueError(msg)
+        if extra_fields.get("is_superuser") is not True:
+            msg = _("Superuser must have is_superuser=True.")
+            raise ValueError(msg)
+
+        return self._create_user(email, password, **extra_fields)
--- users/models.py
+++ users/models.py
@@ -1,16 +1,20 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-
+from .managers import UserManager
+from typing import ClassVar
 
 class CustomUser(AbstractUser):
     username = None
     full_name = models.CharField(max_length=200, unique=False, null=True, blank=True)
+    email = models.EmailField(("email address"), unique=True)
     profile_picture = models.ImageField(
         upload_to="profile_picture", blank=True, null=True
     )
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
+    objects: ClassVar[UserManager] = UserManager()
+
     def __str__(self):
         return str(self.username) if self.username else str(self.email)
+    
