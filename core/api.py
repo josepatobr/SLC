0f29f53e39f9c60538799701cc03c9de22bac91b
josepatobr <josecarlosrodriguesrod.pinto@gmail.com>
2026-02-02 13:02:38 -0300

    adicionando o sistema de IA

--- core/api.py
+++ core/api.py
@@ -1,10 +1,12 @@
 from payment.api.service import stripe_api
 from filmes.api.api import router_movies
 from upload.api import router_upload
+from ia.api.api_ia import router_ia
 from ninja import NinjaAPI
 
 
 api = NinjaAPI()
+api.add_router("ia/", router_ia)
+api.add_router("payment/", stripe_api)
 api.add_router("movies/", router_movies)
 api.add_router("upload/", router_upload)
-api.add_router("payment/", stripe_api)
